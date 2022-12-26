1b695406bfa39598ad78070b7ac2a9163a496b41
Nikhil Kamath B <nikhilkb98@gmail.com>
2022-12-26 02:28:38 -0800

    Connected html templates to backend

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from home.models import *
 
 
 def index(request):
-    return render(request, "home/home.html", {})
+    education = Education.objects.all().order_by("-start_date")
+    work = Work.objects.all().order_by("-start_date")
+    project = Project.objects.all().order_by("-start_date")
+    context = {
+        "education": education,
+        "work": work,
+        "project": project
+    }
+    return render(request, "home/home.html", context)
