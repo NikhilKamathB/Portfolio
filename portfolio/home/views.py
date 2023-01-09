5790a41c704194ae713481aa2e332a43194e6e4c
Nikhil Kamath B <nikhilkb98@gmail.com>
2023-01-09 00:51:37 -0800

    Rand project sequence

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,4 +1,3 @@
-import random
 from django.shortcuts import render
 from home.models import *
 
@@ -6,8 +5,7 @@ from home.models import *
 def index(request):
     education = Education.objects.all().order_by("-start_date")
     work = Work.objects.all().order_by("-start_date")
-    project = Project.objects.all().order_by("-start_date")
-    random.shuffle(project)
+    project = Project.objects.all().order_by('?')
     context = {
         "education": education,
         "work": work,
